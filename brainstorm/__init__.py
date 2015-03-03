4f03c1c51850f8af426700700f62541eca098056
Rupesh Kumar Srivastava <rupesh@idsia.ch>
2015-03-03 20:46:09 +0100

    Reverting previous commit since the problem might be bigger

--- brainstorm/__init__.py
+++ brainstorm/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
-from .structure import *
-from .layers import *
-from .randomness import global_rnd
-from .initializers import *
+from brainstorm.structure import *
+from brainstorm.layers import *
+from brainstorm.randomness import global_rnd
+from brainstorm.initializers import *
 
 __version__ = '0.1'
