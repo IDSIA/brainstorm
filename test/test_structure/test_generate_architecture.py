c4cc9a7a9a14bb9f72c03db557bd67caf716ffa3
Qwlouse <qwlouse@gmail.com>
2015-03-18 16:55:56 +0100

    tests for generating architecture with named inputs and outputs

--- test/test_structure/test_generate_architecture.py
+++ test/test_structure/test_generate_architecture.py
@@ -24,6 +24,23 @@ def test_get_layer_description():
     }
 
 
+def test_get_layer_description_named_inputs_outputs():
+    l = ConstructionWrapper.create('layertype', 10, name='foo')
+    l2 = ConstructionWrapper.create('layertype', 10, name='bar')
+    l3 = ConstructionWrapper.create('layertype', 10, name='baz')
+    _ = l - 'out1' >> l2
+    _ = l >> 'A' - l3
+    descr = get_layer_description(l.layer)
+    assert descr == {
+        '@type': 'layertype',
+        '@outgoing_connections': {
+            'default': {'baz.A'},
+            'out1': {'bar.default'}
+        },
+        'shape': 10
+    }
+
+
 def test_layer_with_kwargs():
     l = ConstructionWrapper.create('layertype', 10, name='foo', a=2, b=3)
     descr = get_layer_description(l.layer)
@@ -41,8 +58,8 @@ def test_generate_architecture():
     l2 = ConstructionWrapper.create('layertype', 20, name='bar')
     l3 = ConstructionWrapper.create('layertype', 30, name='baz')
     l4 = ConstructionWrapper.create('layertype', 40, name='out')
-    _ = l1 >> l2 >> l4
-    _ = l1 >> l3 >> l4
+    _ = l1 - 'foo' >> l2 >> 'A' - l4
+    _ = l1 - 'bar' >> l3 >> 'B' - l4
 
     arch1 = generate_architecture(l1.layer)
     arch2 = generate_architecture(l2.layer)
@@ -54,7 +71,8 @@ def test_generate_architecture():
             '@type': 'DataLayer',
             'shape': 10,
             '@outgoing_connections': {
-                'default': {'bar.default', 'baz.default'},
+                'foo': {'bar.default'},
+                'bar': {'baz.default'},
             }
 
         },
@@ -62,14 +80,14 @@ def test_generate_architecture():
             '@type': 'layertype',
             'shape': 20,
             '@outgoing_connections': {
-                'default': {'out.default'},
+                'default': {'out.A'},
             }
         },
         'baz': {
             '@type': 'layertype',
             'shape': 30,
             '@outgoing_connections': {
-                'default': {'out.default'},
+                'default': {'out.B'},
             }
         },
         'out': {
