89ef505459874a7757eb4d4cd399d8be39bcf899
Qwlouse <qwlouse@gmail.com>
2015-04-11 12:28:25 +0200

    WIP: refactoring layer tests

--- test/test_layers.py
+++ test/test_layers.py
@@ -13,6 +13,7 @@ from .helpers import run_layer_test
 import numpy as np
 from brainstorm.structure.shapes import ShapeTemplate
 from layers.rnn_layer import RnnLayerImpl
+import pytest
 
 np.random.seed(1234)
 
@@ -48,11 +49,10 @@ def test_squared_difference_layer():
     layer.set_handler(NumpyHandler(np.float64))
 
     print("\n---------- Testing SquaredDifferenceLayer ----------")
-    run_layer_test(layer, time_steps, batch_size, eps)
+    # run_layer_test(layer, time_steps, batch_size, eps)
 
 
 def test_binomial_crossentropy_layer():
-
     eps = 1e-5
     time_steps = 3
     batch_size = 2
@@ -66,44 +66,55 @@ def test_binomial_crossentropy_layer():
                  'targets': ShapeTemplate('T', 'B', *feature_shape)}
 
     layer = BinomialCrossEntropyLayerImpl('TestLayer', in_shapes, NO_CON, NO_CON)
-    layer.set_handler(NumpyHandler(np.float64))
-
-    print("\n---------- Testing BinomialCrossEntropyError ----------")
-    run_layer_test(layer, time_steps, batch_size, eps,
-                   skip_inputs=['targets'], default=default, targets=targets)
 
 
-def test_classification_layer():
-    eps = 1e-5
+    #run_layer_test(layer, time_steps, batch_size, eps,
+    #               skip_inputs=['targets'], default=default, targets=targets)
+
+
+def classification_layer():
     time_steps = 3
     batch_size = 2
     feature_dim = 5
     shape = (time_steps, batch_size, 1)
     targets = np.random.randint(0, feature_dim, shape)
-    print(targets)
     in_shapes = {'default': ShapeTemplate('T', 'B', feature_dim),
                  'targets': ShapeTemplate('T', 'B', 1)}
 
-    layer = ClassificationLayerImpl('TestLayer', in_shapes, NO_CON, NO_CON,
-                                    size=feature_dim)
+    layer = ClassificationLayerImpl('ClassificationLayer', in_shapes, NO_CON,
+                                    NO_CON, size=feature_dim)
+    return layer, {
+        'time_steps': time_steps,
+        'batch_size': batch_size,
+        'skip_inputs': ['targets'],
+        'skip_output': ['output'],
+        'targets': targets
+    }
+
+
+def rnn_layer():
+    in_shapes = {'default': ShapeTemplate('T', 'B', 5)}
+    layer = RnnLayerImpl('RnnLayer', in_shapes, NO_CON, NO_CON,
+                         size=7)
+    return layer, {}
+
+layers_to_test = [
+    classification_layer,
+    rnn_layer
+]
+
+ids = [f.__name__ for f in layers_to_test]
+
+
+@pytest.fixture(params=layers_to_test, ids=ids)
+def layer_specs(request):
+    layer, specs = request.param()
+    return layer, specs
+
+
+def test_layer(layer_specs):
+    layer, specs = layer_specs
+    print("\n---------- Testing {} ----------".format(layer.name))
     layer.set_handler(NumpyHandler(np.float64))
 
-    print("\n---------- Testing ClassificationLayer ----------")
-    run_layer_test(layer, time_steps, batch_size, eps,
-                   skip_inputs=['targets'], skip_outputs=['output'],
-                   targets=targets)
-
-
-def test_rnn_layer():
-    eps = 1e-5
-    time_steps = 3
-    batch_size = 2
-    feature_dim = 5
-    in_shapes = {'default': ShapeTemplate('T', 'B', feature_dim)}
-
-    layer = RnnLayerImpl('TestLayer', in_shapes, NO_CON, NO_CON,
-                         size=feature_dim)
-    layer.set_handler(NumpyHandler(np.float64))
-
-    print("\n---------- Testing RnnLayer ----------")
-    run_layer_test(layer, time_steps, batch_size, eps)
+    run_layer_test(layer, 3, 2, 1e-5, **specs)
