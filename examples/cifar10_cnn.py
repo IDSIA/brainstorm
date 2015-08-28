96a72d5bea261ff3d80ddfbf63564d8b6178cb4f
Thomas Unterthiner <unterthiner@bioinf.jku.at>
2015-08-28 10:54:26 +0200

    Update Example

--- examples/cifar10_cnn.py
+++ examples/cifar10_cnn.py
@@ -57,31 +57,31 @@ out = bs.layers.Classification(10, name="out")
 
 # this network is similar to the 'cifar10-quick' example in caffe
 inp >> \
-    bs.layers.Convolution2D(32, kernel_size=(5, 5), pad=2, name='conv1', activation_function='rel') >> \
-    bs.layers.Pooling2D(32, window_size=(3, 3), stride=(2, 2), name='pool1') >> \
-    bs.layers.Convolution2D(32, kernel_size=(5, 5), pad=2, name='conv2', activation_function='rel') >> \
-    bs.layers.Pooling2D(32, window_size=(3, 3), stride=(2, 2), name='pool2') >> \
-    bs.layers.Convolution2D(64, kernel_size=(5, 5), pad=2, name='conv3', activation_function='rel') >> \
-    bs.layers.Pooling2D(64, window_size=(3, 3), stride=(2, 2), name='pool3') >> \
+    bs.layers.Convolution2D(32, kernel_size=(5, 5), padding=2, name='conv1', activation_function='rel') >> \
+    bs.layers.Pooling2D(32, kernel_size=(3, 3), stride=(2, 2), name='pool1') >> \
+    bs.layers.Convolution2D(32, kernel_size=(5, 5), padding=2, name='conv2', activation_function='rel') >> \
+    bs.layers.Pooling2D(32, kernel_size=(3, 3), stride=(2, 2), name='pool2') >> \
+    bs.layers.Convolution2D(64, kernel_size=(5, 5), padding=2, name='conv3', activation_function='rel') >> \
+    bs.layers.Pooling2D(64, kernel_size=(3, 3), stride=(2, 2), name='pool3') >> \
     bs.layers.FullyConnected(64, name='fc1', activation_function='rel') >> \
     out - "loss" >> bs.layers.Loss()
 
 network = bs.Network.from_layer(inp - 'targets' >> 'targets' - out)
 network.set_memory_handler(PyCudaHandler())
-network.initialize(bs.Gaussian(0.01), seed=42)
+network.initialize(bs.Gaussian(0.001), seed=42)
 network.set_weight_modifiers({"out": bs.ConstrainL2Norm(1)})
 
 # ------------------------------ Set up Trainer ----------------------------- #
 
-trainer = bs.Trainer(bs.SgdStep(learning_rate=0.1), double_buffering=False)
+trainer = bs.Trainer(bs.SgdStep(learning_rate=0.01), double_buffering=False)
 trainer.add_hook(bs.hooks.StopAfterEpoch(100))
 trainer.add_hook(bs.hooks.MonitorAccuracy("valid_getter", "out.output",
                                           name="validation accuracy",
                                           verbose=True))
-trainer.add_hook(bs.hooks.SaveBestNetwork("validation accuracy",
-                                          filename='mnist_lstm_best.hdf5',
-                                          name="best weights",
-                                          criterion="max"))
+#trainer.add_hook(bs.hooks.SaveBestNetwork("validation accuracy",
+#                                          filename='cifar10_cnn_best.hdf5',
+#                                          name="best weights",
+#                                          criterion="max"))
 
 # --------------------------------- Train ----------------------------------- #
 
