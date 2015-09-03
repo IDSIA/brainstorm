f69222fb9d40e5f76ab2ef8f87257a8323e093fe
Rupesh Kumar Srivastava <rupspace@gmail.com>
2015-09-03 14:43:23 +0200

    Added tools.draw_network

--- brainstorm/tools.py
+++ brainstorm/tools.py
@@ -3,7 +3,7 @@
 from __future__ import division, print_function, unicode_literals
 from brainstorm import layers
 
-__all__ = ['get_in_out_layers_for_classification']
+__all__ = ['get_in_out_layers_for_classification', 'draw_network']
 
 
 def get_in_out_layers_for_classification(in_shape, nr_classes,
@@ -31,3 +31,22 @@ def get_in_out_layers_for_classification(in_shape, nr_classes,
         inp_layer - mask_name >> 'mask' - mask_layer
 
     return inp_layer, out_layer
+
+
+def draw_network(network, filename='network.png'):
+    try:
+        import pydot
+        graph = pydot.Dot(graph_type='digraph')
+
+        for k, v in network.architecture.items():
+            for out_view, dest_list in v['@outgoing_connections'].items():
+                for dest in dest_list:
+                    edge = pydot.Edge(k, dest.split('.')[0])
+                    graph.add_edge(edge)
+
+        graph.write_png(filename)
+        print('Network drawing saved as {}'.format(filename))
+
+    except ImportError:
+        print("pydot is required for drawing networks but was not found.")
+
