76d30bbe89b6a3e2488f026140babb52bbeac66f
julian121266 <rtZ678Iob>
2015-10-08 14:44:31 +0200

    optimized LSTM implementation (cherry picked from commit 28d6c1b)

--- brainstorm/layers/lstm_layer.py
+++ brainstorm/layers/lstm_layer.py
@@ -8,7 +8,9 @@ from brainstorm.layers.base_layer import BaseLayerImpl
 from brainstorm.structure.buffer_structure import (BufferStructure,
                                                    StructureTemplate)
 from brainstorm.structure.construction import ConstructionWrapper
-from brainstorm.utils import LayerValidationError
+from brainstorm.utils import LayerValidationError, flatten_time
+from brainstorm.layers.base_layer import LayerBaseImpl
+from brainstorm.structure.shapes import ShapeTemplate
 
 
 def Lstm(size, activation='tanh', name=None):
@@ -85,7 +87,7 @@ class LstmLayerImpl(BaseLayerImpl):
     def set_handler(self, new_handler):
         super(LstmLayerImpl, self).set_handler(new_handler)
 
-        # Assign act_func and act_dunc_derivs
+        # Assign act_func and act_func_derivs
         activations = {
             'sigmoid': (self.handler.sigmoid, self.handler.sigmoid_deriv),
             'tanh': (self.handler.tanh, self.handler.tanh_deriv),
@@ -110,21 +112,28 @@ class LstmLayerImpl(BaseLayerImpl):
 
         time_size, batch_size, in_size = x.shape
 
+        flat_x = flatten_time(x)
+        flat_Za = flatten_time(Za[:-1])
+        flat_Ia = flatten_time(Ia[:-1])
+        flat_Fa = flatten_time(Fa[:-1])
+        flat_Oa = flatten_time(Oa[:-1])
+        _h.dot_mm(flat_x, Wz, flat_Za, transb=True)
+        _h.dot_mm(flat_x, Wi, flat_Ia, transb=True)
+        _h.dot_mm(flat_x, Wf, flat_Fa, transb=True)
+        _h.dot_mm(flat_x, Wo, flat_Oa, transb=True)
+
         for t in range(time_size):
             # Block input
-            _h.dot_mm(x[t], Wz, Za[t], transb=True)
             _h.dot_add_mm(y[t - 1], Rz, Za[t])
             _h.add_mv(Za[t], bz.reshape((1, self.size)), Za[t])
             self.act_func(Za[t], Zb[t])
 
             # Input Gate
-            _h.dot_mm(x[t], Wi, Ia[t], transb=True)
             _h.dot_add_mm(y[t - 1], Ri, Ia[t])
             _h.add_mv(Ia[t], bi.reshape((1, self.size)), Ia[t])
             _h.sigmoid(Ia[t], Ib[t])
 
             # Forget Gate
-            _h.dot_mm(x[t], Wf, Fa[t], transb=True)
             _h.dot_add_mm(y[t - 1], Rf, Fa[t])
             _h.add_mv(Fa[t], bf.reshape((1, self.size)), Fa[t])
             _h.sigmoid(Fa[t], Fb[t])
@@ -134,7 +143,6 @@ class LstmLayerImpl(BaseLayerImpl):
             _h.mult_add_tt(Fb[t], Ca[t - 1], Ca[t])
 
             # Output Gate
-            _h.dot_mm(x[t], Wo, Oa[t], transb=True)
             _h.dot_add_mm(y[t - 1], Ro, Oa[t])
             _h.add_mv(Oa[t], bo.reshape((1, self.size)), Oa[t])
             _h.sigmoid(Oa[t], Ob[t])
@@ -193,31 +201,48 @@ class LstmLayerImpl(BaseLayerImpl):
             _h.mult_tt(dCa[t], Ib[t], dZb[t])
             self.act_func_deriv(Za[t], Zb[t], dZb[t], dZa[t])
 
-            # Input Deltas
-            _h.dot_add_mm(dIa[t], Wi, dx[t])
-            _h.dot_add_mm(dFa[t], Wf, dx[t])
-            _h.dot_add_mm(dOa[t], Wo, dx[t])
-            _h.dot_add_mm(dZa[t], Wz, dx[t])
+        # Same as for standard RNN:
+        flat_inputs = flatten_time(x)
+        flat_dinputs = flatten_time(dx)
 
-            # Gradients for the input weights
-            _h.dot_add_mm(dIa[t], x[t], dWi, transa=True)
-            _h.dot_add_mm(dFa[t], x[t], dWf, transa=True)
-            _h.dot_add_mm(dOa[t], x[t], dWo, transa=True)
-            _h.dot_add_mm(dZa[t], x[t], dWz, transa=True)
+        flat_dIa = flatten_time(dIa[:-1])
+        flat_dFa = flatten_time(dFa[:-1])
+        flat_dOa = flatten_time(dOa[:-1])
+        flat_dZa = flatten_time(dZa[:-1])
 
-            # Gradient for the recurrent weights
-            _h.dot_add_mm(y[t], dIa[t + 1], dRi, transa=True)
-            _h.dot_add_mm(y[t], dFa[t + 1], dRf, transa=True)
-            _h.dot_add_mm(y[t], dOa[t + 1], dRo, transa=True)
-            _h.dot_add_mm(y[t], dZa[t + 1], dRz, transa=True)
+        # calculate in_deltas and gradients
+        _h.dot_add_mm(flat_dIa, Wi, flat_dinputs)
+        _h.dot_add_mm(flat_dFa, Wf, flat_dinputs)
+        _h.dot_add_mm(flat_dOa, Wo, flat_dinputs)
+        _h.dot_add_mm(flat_dZa, Wz, flat_dinputs)
 
-            # biases
-            bias_tmp = _h.allocate(dbz.shape)
-            _h.sum_t(dIa[t], axis=0, out=bias_tmp)
-            _h.add_tt(bias_tmp, dbi, dbi)
-            _h.sum_t(dFa[t], axis=0, out=bias_tmp)
-            _h.add_tt(bias_tmp, dbf, dbf)
-            _h.sum_t(dOa[t], axis=0, out=bias_tmp)
-            _h.add_tt(bias_tmp, dbo, dbo)
-            _h.sum_t(dZa[t], axis=0, out=bias_tmp)
-            _h.add_tt(bias_tmp, dbz, dbz)
+        _h.dot_add_mm(flat_dIa, flat_inputs, dWi, transa=True)
+        _h.dot_add_mm(flat_dFa, flat_inputs, dWf, transa=True)
+        _h.dot_add_mm(flat_dOa, flat_inputs, dWo, transa=True)
+        _h.dot_add_mm(flat_dZa, flat_inputs, dWz, transa=True)
+
+        dbias_tmp = _h.allocate(dbz.shape)
+        _h.sum_t(flat_dIa, axis=0, out=dbias_tmp)
+        _h.add_tt(dbi, dbias_tmp, dbi)
+        _h.sum_t(flat_dFa, axis=0, out=dbias_tmp)
+        _h.add_tt(dbf, dbias_tmp, dbf)
+        _h.sum_t(flat_dOa, axis=0, out=dbias_tmp)
+        _h.add_tt(dbo, dbias_tmp, dbo)
+        _h.sum_t(flat_dZa, axis=0, out=dbias_tmp)
+        _h.add_tt(dbz, dbias_tmp, dbz)
+
+        flat_outputs = flatten_time(y[:-2])
+        flat_dIa = flatten_time(dIa[1:-1])
+        flat_dFa = flatten_time(dFa[1:-1])
+        flat_dOa = flatten_time(dOa[1:-1])
+        flat_dZa = flatten_time(dZa[1:-1])
+
+        _h.dot_add_mm(flat_outputs, flat_dIa, dRi, transa=True)
+        _h.dot_add_mm(flat_outputs, flat_dFa, dRf, transa=True)
+        _h.dot_add_mm(flat_outputs, flat_dOa, dRo, transa=True)
+        _h.dot_add_mm(flat_outputs, flat_dZa, dRz, transa=True)
+
+        _h.dot_add_mm(dy[-1], dIa[0], dRi, transa=True)
+        _h.dot_add_mm(dy[-1], dFa[0], dRf, transa=True)
+        _h.dot_add_mm(dy[-1], dOa[0], dRo, transa=True)
+        _h.dot_add_mm(dy[-1], dZa[0], dRz, transa=True)
