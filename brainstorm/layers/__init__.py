5fa8024bf620aa1b7fbb51ae14a38d9b838db6b1
Rupesh Kumar Srivastava <rupesh@idsia.ch>
2015-10-20 20:50:52 +0200

    Removed a couple of redundant imports

--- brainstorm/layers/__init__.py
+++ brainstorm/layers/__init__.py
@@ -27,5 +27,3 @@ from brainstorm.layers.recurrent_layer import Recurrent
 from brainstorm.layers.sigmoid_ce_layer import SigmoidCE
 from brainstorm.layers.softmax_ce_layer import SoftmaxCE
 from brainstorm.layers.squared_difference_layer import SquaredDifference
-from brainstorm.layers.l1_decay import L1Decay
-from brainstorm.layers.l2_decay import L2Decay
