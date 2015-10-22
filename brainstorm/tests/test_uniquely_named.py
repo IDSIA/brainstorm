ac599f43921ff93658726832c7b61ba5abd06373
Qwlouse <qwlouse@gmail.com>
2015-10-22 20:01:57 +0200

    moved UniquelyNamed into construction

--- brainstorm/tests/test_uniquely_named.py
+++ brainstorm/tests/test_uniquely_named.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
-
-from brainstorm.uniquely_named import UniquelyNamed
+from structure.construction import UniquelyNamed
 
 
 def test_basename():
