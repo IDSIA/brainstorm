45c698ea6d8cad360bc51b410c864cb8f484cb11
Qwlouse <qwlouse@gmail.com>
2015-11-03 16:31:12 +0100

    Make SquaredDifference not sum over feature dimensions

--- brainstorm/layers/squared_difference_layer.py
+++ brainstorm/layers/squared_difference_layer.py
@@ -8,8 +8,7 @@ from brainstorm.layers.base_layer import Layer
 from brainstorm.structure.buffer_structure import (BufferStructure,
                                                    StructureTemplate)
 from brainstorm.structure.construction import ConstructionWrapper
-from brainstorm.utils import (LayerValidationError, flatten_features,
-                              flatten_time, flatten_time_and_features)
+from brainstorm.utils import (LayerValidationError, flatten_time_and_features)
 
 
 def SquaredDifference(name=None):
@@ -30,18 +29,14 @@ class SquaredDifferenceLayerImpl(Layer):
         if f_size1 != f_size2:
             raise LayerValidationError(
                 "{}: inputs_1 and inputs_2 must have same feature sizes but "
-                "got {} and {}".format(self.name,
-                                       in_shapes['inputs_1'].feature_shape,
-                                       in_shapes['inputs_2'].feature_shape))
+                "got {} and {}".format(self.name, f_size1, f_size2))
 
         outputs = OrderedDict()
-        outputs['default'] = BufferStructure('T', 'B', 1)
+        outputs['default'] = BufferStructure('T', 'B', f_size1)
 
         internals = OrderedDict()
         feature_size = self.in_shapes['inputs_1'].feature_size
-        internals['squared_diff'] = BufferStructure('T', 'B', feature_size)
-        internals['grad_diff'] = BufferStructure('T', 'B', feature_size,
-                                                 is_backward_only=True)
+        internals['diff'] = BufferStructure('T', 'B', feature_size)
         return outputs, OrderedDict(), internals
 
     def forward_pass(self, buffers, training_pass=True):
@@ -49,34 +44,24 @@ class SquaredDifferenceLayerImpl(Layer):
         _h = self.handler
         inputs_1 = flatten_time_and_features(buffers.inputs.inputs_1)
         inputs_2 = flatten_time_and_features(buffers.inputs.inputs_2)
-        diff = flatten_time_and_features(buffers.internals.squared_diff)
-        diff_sum = flatten_time(buffers.outputs.default)
+        diff = flatten_time_and_features(buffers.internals.diff)
+        outputs = flatten_time_and_features(buffers.outputs.default)
 
         # calculate
         _h.subtract_tt(inputs_1, inputs_2, out=diff)
-        _h.mult_tt(diff, diff, out=diff)
-        _h.sum_t(diff, axis=1, out=diff_sum)
-        _h.mult_st(0.5, diff_sum, out=diff_sum)
+        _h.mult_tt(diff, diff, out=outputs)
+        _h.mult_st(0.5, outputs, out=outputs)
 
     def backward_pass(self, buffers):
         # prepare
         _h = self.handler
-        inputs_1 = flatten_time_and_features(buffers.inputs.inputs_1)
-        inputs_2 = flatten_time_and_features(buffers.inputs.inputs_2)
-        out_deltas = buffers.output_deltas.default
-        grad_diff = buffers.internals.grad_diff
+        out_deltas = flatten_time_and_features(buffers.output_deltas.default)
+        diff = flatten_time_and_features(buffers.internals.diff)
         dinputs_1 = flatten_time_and_features(buffers.input_deltas.inputs_1)
         dinputs_2 = flatten_time_and_features(buffers.input_deltas.inputs_2)
 
-        tmp = _h.allocate(inputs_2.shape)
-        # out_deltas has only one feature dimension due to summation,
-        # so we broadcast to all feature dimensions
-        _h.broadcast_t(out_deltas, 2, grad_diff)
-
-        grad_diff = flatten_time(grad_diff)
+        tmp = _h.allocate(out_deltas.shape)
         # calculate
-        _h.subtract_tt(inputs_1, inputs_2, out=tmp)
-        _h.mult_add_tt(grad_diff, tmp, dinputs_1)
-
-        _h.subtract_tt(inputs_2, inputs_1, out=tmp)
-        _h.mult_add_tt(grad_diff, tmp, dinputs_2)
+        _h.mult_add_tt(out_deltas, diff, dinputs_1)
+        _h.mult_st(-1, diff, out=tmp)
+        _h.mult_add_tt(out_deltas, tmp, dinputs_2)
