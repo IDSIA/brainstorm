271987a1ea329a52606e0545f93b573457a3a4cb
Rupesh Kumar Srivastava <rupspace@gmail.com>
2015-11-04 15:54:52 +0100

    SquaredDifference layer does not scale by half anymore. Additionally, its internal diff maintains shape.

--- brainstorm/layers/squared_difference_layer.py
+++ brainstorm/layers/squared_difference_layer.py
@@ -24,19 +24,19 @@ class SquaredDifferenceLayerImpl(Layer):
 
     def setup(self, kwargs, in_shapes):
         # 'inputs_1' and 'inputs_2' must have same shape
-        f_size1 = in_shapes['inputs_1'].feature_size
-        f_size2 = in_shapes['inputs_2'].feature_size
-        if f_size1 != f_size2:
+        f_shape1 = in_shapes['inputs_1'].feature_shape
+        f_shape2 = in_shapes['inputs_2'].feature_shape
+        if f_shape1 != f_shape2:
             raise LayerValidationError(
-                "{}: inputs_1 and inputs_2 must have same feature sizes but "
-                "got {} and {}".format(self.name, f_size1, f_size2))
+                "{}: inputs_1 and inputs_2 must have same feature shapes but "
+                "got {} and {}".format(self.name, f_shape1, f_shape2))
 
         outputs = OrderedDict()
-        outputs['default'] = BufferStructure('T', 'B', f_size1)
+        outputs['default'] = BufferStructure('T', 'B', *f_shape1)
 
         internals = OrderedDict()
-        feature_size = self.in_shapes['inputs_1'].feature_size
-        internals['diff'] = BufferStructure('T', 'B', feature_size)
+        feature_shape = self.in_shapes['inputs_1'].feature_shape
+        internals['diff'] = BufferStructure('T', 'B', *feature_shape)
         return outputs, OrderedDict(), internals
 
     def forward_pass(self, buffers, training_pass=True):
@@ -50,7 +50,6 @@ class SquaredDifferenceLayerImpl(Layer):
         # calculate
         _h.subtract_tt(inputs_1, inputs_2, out=diff)
         _h.mult_tt(diff, diff, out=outputs)
-        _h.mult_st(0.5, outputs, out=outputs)
 
     def backward_pass(self, buffers):
         # prepare
@@ -62,6 +61,7 @@ class SquaredDifferenceLayerImpl(Layer):
 
         tmp = _h.allocate(out_deltas.shape)
         # calculate
-        _h.mult_add_tt(out_deltas, diff, dinputs_1)
+        _h.mult_st(2, out_deltas, out=out_deltas)
+        _h.mult_add_tt(out_deltas, diff, out=dinputs_1)
         _h.mult_st(-1, diff, out=tmp)
-        _h.mult_add_tt(out_deltas, tmp, dinputs_2)
+        _h.mult_add_tt(out_deltas, tmp, out=dinputs_2)
