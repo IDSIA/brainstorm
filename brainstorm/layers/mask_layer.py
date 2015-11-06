08efb72aab0b4f86f23e12609303c18ef133e1e0
Qwlouse <qwlouse@gmail.com>
2015-11-06 16:41:51 +0100

    made mask layer even more flexible
    for input shape of (T, B, 5, 3, 2) the mask shape can now be one of (T, B, 1), (T, B, 5, 3, 1) and (T, B, 5, 3, 2).

--- brainstorm/layers/mask_layer.py
+++ brainstorm/layers/mask_layer.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 from brainstorm.layers.base_layer import Layer
 from brainstorm.structure.buffer_structure import StructureTemplate
 from brainstorm.structure.construction import ConstructionWrapper
-from brainstorm.utils import flatten_time_and_features, LayerValidationError
+from brainstorm.utils import LayerValidationError, product
 
 
 def Mask(name=None):
@@ -24,33 +24,44 @@ class MaskLayerImpl(Layer):
 
     def setup(self, kwargs, in_shapes):
         in_shape = in_shapes['default'].feature_shape
-        if in_shapes['mask'].feature_shape not in [(1,), in_shape]:
+        expected_shape = in_shape[:-1] + (1,)
+
+        if in_shapes['mask'].feature_shape == (1,):
+            self.flatten_dim = 2
+        elif in_shapes['mask'].feature_shape in [expected_shape, in_shape]:
+            self.flatten_dim = len(in_shape) + 1
+        else:
             raise LayerValidationError(
                 "Shape of the mask did not match shape of the default inputs. "
-                "Should be either ('T', 'B', 1) or {}, but was {}".format(
-                    in_shapes['default'].shape), in_shapes['mask'].shape)
+                "Should be either ('T', 'B', 1) or {} or {}, but was {}".format(
+                    ('T', 'B') + expected_shape,
+                    in_shapes['default'].shape,
+                    in_shapes['mask']))
         outputs = OrderedDict()
         outputs['default'] = in_shapes['default']
         return outputs, OrderedDict(), OrderedDict()
 
+    def flatten_buffer(self, buffer):
+        pre = buffer.shape[:self.flatten_dim]
+        post = buffer.shape[self.flatten_dim:]
+        return buffer.reshape((int(product(pre)), int(product(post))))
+
     def forward_pass(self, buffers, training_pass=True):
         _h = self.handler
 
-        flat_inp = flatten_time_and_features(buffers.inputs.default)
-        flat_mask = flatten_time_and_features(buffers.inputs.mask)
-        flat_out = flatten_time_and_features(buffers.outputs.default)
+        flat_inp = self.flatten_buffer(buffers.inputs.default)
+        flat_mask = self.flatten_buffer(buffers.inputs.mask)
+        flat_out = self.flatten_buffer(buffers.outputs.default)
 
         _h.mult_mv(flat_inp, flat_mask, out=flat_out)
 
     def backward_pass(self, buffers):
         _h = self.handler
 
-        flat_out_deltas = flatten_time_and_features(
-            buffers.output_deltas.default)
+        flat_out_deltas = self.flatten_buffer(buffers.output_deltas.default)
         tmp = self.handler.allocate(flat_out_deltas.shape)
-        flat_mask = flatten_time_and_features(buffers.inputs.mask)
-        flat_in_deltas = flatten_time_and_features(
-            buffers.input_deltas.default)
+        flat_mask = self.flatten_buffer(buffers.inputs.mask)
+        flat_in_deltas = self.flatten_buffer(buffers.input_deltas.default)
 
         _h.mult_mv(flat_out_deltas, flat_mask, tmp)
         _h.add_tt(tmp, flat_in_deltas, flat_in_deltas)
