ba4393837b326c04081d2d4f2107a1ff667a496b
Andi Heinrich <git@wafna.de>
2016-04-19 15:11:10 +0200

    added missing return statement in FullyConnected construction wrapper

--- brainstorm/layers/fully_connected_layer.py
+++ brainstorm/layers/fully_connected_layer.py
@@ -15,8 +15,8 @@ from brainstorm.utils import (LayerValidationError, flatten_time,
 def FullyConnected(size=None, activation='rel', name=None):
     """Create a Fully Connected (inner product) layer."""
     if size is None:
-        ConstructionWrapper.create(FullyConnectedLayerImpl, name=name,
-                                   activation=activation)
+        return ConstructionWrapper.create(FullyConnectedLayerImpl, name=name,
+                                          activation=activation)
     else:
         return ConstructionWrapper.create(FullyConnectedLayerImpl, size=size,
                                           name=name, activation=activation)
