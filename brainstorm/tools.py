ffe32202239d804aa7fc7c12cc0f720e6c2ce92c
miguel <miguellissimo@gmail.com>
2017-01-02 10:45:16 +0100

    indexing on dictionary view object bugfix

--- brainstorm/tools.py
+++ brainstorm/tools.py
@@ -103,7 +103,7 @@ def extract(network, iter, buffer_names):
     if isinstance(buffer_names, six.string_types):
         buffer_names = [buffer_names]
 
-    nr_examples = iter.data_shapes.values()[0][1]
+    nr_examples = list(iter.data_shapes.values())[0][1]
     return_data = {}
     nr_items = 0
     for _ in run_network(network, iterator, all_inputs=False):
